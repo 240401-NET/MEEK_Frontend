e424d2c86a4e54c5cdbc5df58971f7b5686a43fa
Michael Phan <michaelphan35@gmail.com>
2024-04-25 15:10:25 -0700

    configured button functionality

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,33 @@ import './App.css'
 
 function App() {
 
+
+
+const signinClick = () => {
+let signupBtn = document.getElementById("signupBtn") as HTMLButtonElement;
+let signinBtn = document.getElementById("signinBtn")! as HTMLButtonElement;
+let title = document.getElementById('title')! as HTMLElement;
+let nameField = document.getElementById("nameField")! as HTMLElement;
+  // signinBtn?.style.setProperty("background", "green");
+  nameField?.style.setProperty("max-height", "0");
+  title.innerHTML = 'Sign In';
+  signupBtn.classList.add("disable");
+  signinBtn.classList.remove("disable");
+}
+
+
+const signupClick = () => {
+  let signupBtn = document.getElementById("signupBtn") as HTMLButtonElement;
+  let signinBtn = document.getElementById("signinBtn")! as HTMLButtonElement;
+  let title = document.getElementById('title')! as HTMLElement;
+  let nameField = document.getElementById("nameField")! as HTMLElement;
+    // signinBtn?.style.setProperty("background", "green");
+    nameField?.style.setProperty("max-height", "65px");
+    title.innerHTML = 'Sign Up';
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+  }
+  
   return (
     <div>
     <div className="container">
@@ -26,8 +53,8 @@ function App() {
             <p>Lost Password <a href="#">Click Here!</a></p>
             </div>
             <div className="btn-field"> 
-              <button type="button" id="signupBtn">Sign Up</button>
-              <button type="button" id="signinBtn" className="disable">Sign In</button>
+              <button type="button" id="signupBtn" onClick={()=>signupClick()}>Sign Up</button>
+              <button type="button" id="signinBtn" className="disable" onClick={()=>signinClick()}>Sign In</button>
             </div>
           </form>
       </div>
@@ -35,5 +62,20 @@ function App() {
     </div>
   )
 }
+
+// let signinBtn : HTMLElement = document.getElementById("signinBtn")!;
+// let nameField : HTMLElement = document.getElementById("nameField")!;
+// let title = document.getElementById("title")!;
+// function click (){
+
+// }
+
+// signupBtn.onclick = ()=>{
+//   nameField.style.maxHeight = "60px";
+//   title.innerHTML = "Sign Up";
+//   signupBtn.classList.remove("disable");
+//   signinBtn.classList.add("disable");
+// }
+
 
 export default App
