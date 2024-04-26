00d3e1dd7c9759c0a293664f506154d598a0e788
Michael Phan <michaelphan35@gmail.com>
2024-04-26 10:08:03 -0700

    created rough outline for trainers page, established moving between pages added notes on what needs to be done functionality wise

---
+++ vite-project/src/components/TeamBuilder.tsx
@@ -0,0 +1,17 @@
+import './TeamBuilder.css'
+import {NavLink as Link} from "react-router-dom"
+
+export function TeamBuilder()
+{
+    return (
+        <div>
+            <p>WOO linking worked</p>
+            <Link to="/trainer">
+                <button>Show all trainer teams</button>
+            </Link>
+            <Link to="/">
+                <button>Logout</button>
+            </Link>
+        </div>
+    )
+}
---
+++ vite-project/src/components/TrainerPage.tsx
@@ -0,0 +1,56 @@
+import './TrainerPage.css'
+import {NavLink as Link} from "react-router-dom"
+import { useState, useEffect} from 'react';
+
+export function TrainerPage()
+{
+    const [name, setName] = useState("");
+
+    useEffect(()=> {
+        // create call to get trainer name;
+        // pass that name into the setName (useState function)
+        setName("Red")
+    })
+    
+    return (
+        <div id="trainerContainer">
+            <div id="trainerHeadingDiv">
+                <p id='trainerid' >{name}</p>
+                <Link to="/">
+                    <button>Logout</button>
+                </Link>
+            </div>
+            
+            {/* need to create functions that uses team id and loads sprites, and names of each pokemon on team */}
+            <div className="trainerTeamContainer" >
+                <div className="teamMember">
+                    <p>team member 1</p>
+                </div>
+                <div className="teamMember">
+                    <p>team member 2</p>
+                </div>
+                <div className="teamMember">
+                    <p>team member 3</p>
+                </div>
+                <div className="teamMember">
+                    <p>team member 4</p>
+                </div>
+                <div className="teamMember">
+                    <p>team member 5</p>
+                </div>
+                <div className="teamMember">
+                    <p>team member 6</p>
+                </div>
+                <div className='teamOptionsDiv'>
+                    <Link to="/teambuilder">
+                        <button type="button">Create new team</button>
+                    </Link>
+                    {/* Need function to handle onclick event for deleting team based of trainer id */}
+                    <button type="button">Delete Team</button>
+                </div>
+
+                {/* Need function to auto generate the above div layout with specific  */}
+            </div>
+        </div>
+    )
+}
