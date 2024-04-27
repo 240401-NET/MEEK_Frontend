32ed9ba0c934f5d39a7fbb04277e35ed8777510d
Michael Phan <michaelphan35@gmail.com>
2024-04-26 17:26:42 -0700

    refactored plain index html to react typescript

--- vite-project/src/components/TrainerPage.tsx
+++ vite-project/src/components/TrainerPage.tsx
@@ -1,66 +1,138 @@
 import './TrainerPage.css';
-import './TrainerPageLogic';
-import {NavLink as Link} from "react-router-dom"
-import { useState, useEffect} from 'react';
+import Search from '../assets/search.png';
+import JSON from '../assets/json.png';
+// import './TrainerPageLogic';
+// import {NavLink as Link} from "react-router-dom"
+// import { useState, useEffect} from 'react';
 
 export function TrainerPage()
 {
-    
-    // const [name, setName] = useState("");
 
-    // useEffect(()=> {
-    //     // create call to get trainer name;
-    //     // pass that name into the setName (useState function)
-    //     setName("Red")
-    // })
-    
     return (
-        
-        // <div id="trainerContainer">
-        //     <div id="trainerHeadingDiv">
-        //         <p id='trainerid' ></p>
-        //         <Link to="/">
-        //             <button>Logout</button>
-        //         </Link>
-        //     </div>
-            
-        //     {/* need to create functions that uses team id and loads sprites, and names of each pokemon on team */}
-        //     <div className="trainerTeamContainer" >
-        //         <div className="teamMember">
-        //             <p>team member 1</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className="teamMember">
-        //             <p>team member 2</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className="teamMember">
-        //             <p>team member 3</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className="teamMember">
-        //             <p>team member 4</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className="teamMember">
-        //             <p>team member 5</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className="teamMember">
-        //             <p>team member 6</p>
-        //             <p>sprite:</p>
-        //         </div>
-        //         <div className='teamOptionsDiv'>
-        //             <Link to="/teambuilder">
-        //                 <button type="button">Create new team</button>
-        //             </Link>
-        //             {/* Need function to handle onclick event for deleting team based of trainer id */}
-        //             <button type="button">Delete Team</button>
-        //         </div>
-
-        //         {/* Need function to auto generate the above div layout with specific  */}
-        //     </div>
-        // </div>
-        <p>Well this is going to take some time</p>
+        <div>
+            <main className="table" id="pokemon_table">
+                <section className="table__header">
+                    <h1>Pokemon Teams</h1>
+                    <div className="input-group">
+                        <input type="search" placeholder='SEARCH TEAM BY ID/NAME' />
+                        <img src={Search} alt=""/>
+                    </div>
+                    <div className="export__file">
+                        <label htmlFor="export-file" className='export__file-btn' title='Export File'></label>
+                        <input type="checkbox" id="export-file" />
+                        <div className="export__file-options">
+                            <label>Export / Download As &nbsp; &#10140;</label>
+                            <label htmlFor="export-file" id="toJSON">JSON<img src={JSON} alt=""/></label>
+                        </div>
+                    </div>
+                </section>
+                <section className="table__body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Pokemon Id<span className="icon-arrow">&#8593;</span></th>
+                                <th>Pokemon Name<span className="icon-arrow">&#8593;</span></th>
+                                <th>Type<span className="icon-arrow">&#8593;</span></th>
+                                <th>Ability<span className="icon-arrow">&#8593;</span></th>
+                                <th>Egg Group<span className="icon-arrow">&#8593;</span></th>
+                                <th>Move List<span className="icon-arrow">&#8593;</span></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td><a href="https://pokemondb.net/pokedex/charizard"><img src="https://img.pokemondb.net/sprites/lets-go-pikachu-eevee/normal/charizard.png" alt="Charizard"/></a> Charizard</td>
+                                <td>Fire / Flying</td>
+                                <td>Blaze</td>
+                                <td>
+                                    <p className="egg-group dragon">Dragon</p>
+                                </td>
+                                <td>
+                                     Move 1
+                                <br/>Move 2
+                                <br/>Move 3
+                                <br/>Move 4
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 2</td>
+                                <td><a href="https://pokemondb.net/pokedex/swampert"><img src="https://img.pokemondb.net/sprites/black-white/normal/swampert.png" alt="Swampert"/></a>Swampert</td>
+                                <td> Type 1/ Type 2 </td>
+                                <td> Ability </td>
+                                <td>
+                                    <p className="egg-group water1">Water1</p>
+                                </td>
+                                <td>    
+                                     Move 1
+                                <br/>Move 2
+                                <br/>Move 3
+                                <br/>Move 4
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 3</td>
+                                <td><a href="https://pokemondb.net/pokedex/tyranitar"><img src="https://img.pokemondb.net/sprites/diamond-pearl/normal/tyranitar.png" alt="Tyranitar"/></a>Tyranitar</td>
+                                <td> Type 1/ Type 2 </td>
+                                <td> Ability </td>
+                                <td>
+                                    <p className="egg-group field">Field</p>
+                                </td>
+                                <td>    
+                                         Move 1
+                                    <br/>Move 2
+                                    <br/>Move 3
+                                    <br/>Move 4
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 4</td>
+                                <td><a href="https://pokemondb.net/pokedex/empoleon"><img src="https://img.pokemondb.net/sprites/x-y/normal/empoleon.png" alt="Empoleon"/></a>Empoleon</td>
+                                <td> Type 1/ Type 2 </td>
+                                <td> Ability </td>
+                                <td>
+                                    <p className="egg-group water2">Water2</p>
+                                </td>
+                                <td>    
+                                    Move 1
+                                <br/>Move 2
+                                <br/>Move 3
+                                <br/> Move 4
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 5</td>
+                                <td><a href="https://pokemondb.net/pokedex/incineroar"><img src="https://img.pokemondb.net/sprites/sword-shield/normal/incineroar.png" alt="Incineroar"/></a> Incineroar</td>
+                                <td> Type 1/ Type 2 </td>
+                                <td> Ability </td>
+                                <td>
+                                    <p className="egg-group human-like">Human-Like</p>
+                                </td>
+                                <td>    
+                                     Move 1
+                                <br/>Move 2
+                                <br/>Move 3
+                                <br/>Move 4
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 6</td>
+                                <td><a href="https://pokemondb.net/pokedex/mew"><img src="https://img.pokemondb.net/sprites/lets-go-pikachu-eevee/normal/mew.png" alt="Mew"/></a> Mew</td>
+                                <td> Type 1/ Type 2 </td>
+                                <td> Ability </td>
+                                <td>
+                                    <p className="egg-group undiscovered">Undiscovered</p>
+                                </td>
+                                <td>    
+                                    Move 1
+                                <br/>Move 2
+                                <br/>Move 3
+                                <br/>Move 4
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </div>
     )
 }
