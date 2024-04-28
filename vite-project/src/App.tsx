35abb9667950182c509ad3ce7e00229b4a810d93
Michael Phan <michaelphan35@gmail.com>
2024-04-28 09:22:21 -0700

    rebuilt project

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,7 +4,7 @@ import { HomePage } from './components/HomePage';
 import { TeamBuilder } from './components/TeamBuilder';
 import { TrainerPage } from './components/TrainerPage';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import DisplayPokemon from './components/testComponents/displayPokemon.tsx';
+import DisplayPokemon from './components/testComponents/DisplayPokemon.tsx';
 
 function App() {
 
