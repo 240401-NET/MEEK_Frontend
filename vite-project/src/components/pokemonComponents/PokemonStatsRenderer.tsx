e33cdd970f869947bcc29db87c9636ea8a81121e
Michael Phan <michaelphan35@gmail.com>
2024-04-28 19:42:49 -0700

    iv and ev information dynamically changes the pokemon stats

--- vite-project/src/components/pokemonComponents/PokemonStatsRenderer.tsx
+++ vite-project/src/components/pokemonComponents/PokemonStatsRenderer.tsx
@@ -46,43 +46,63 @@ const PokemonStatsRenderer : React.FC<StatsProp> = ({stats}) => {
         { name: 'Timid', increasedStat: 'speed', decreasedStat: 'attack' },
     ]
     const [pokemonLevel, setPokemonLevel] = useState<number>(50);
-    // const [currentNature, setCurrentNature] = useState<string>('Adamamant')
+    const [currentNature, setCurrentNature] = useState<string>('')
+    const [ivs, setIVS] = useState<Record<string, number>>({})
+    const [evs, setEVS] = useState<Record<string, number>>({})
 
-    const calulateBaseStatsWithNatures = (baseStats : Stat[], level :number, nature: Nature | null) => {
+    const calulateBaseStatsWithNatures = (baseStats : Stat[], level :number, nature: string) => {
         return baseStats.map(stat => {
+            const iv = ivs[stat.stat.name]
+            const ev = evs[stat.stat.name]
+
             let baseStat: number = stat.base_stat;
             if (stat.stat.name === 'hp') {
-                baseStat = Math.floor((stat.base_stat * 2 + 31 + (100 / 4)) * level / 100 + 10 + level);
+                baseStat = Math.floor((stat.base_stat * 2 + iv + (ev / 4)) * level / 100 + 10 + level);
             } else {
                 let multiplier : number = 1.0;
-                if (nature) {
-                    if (stat.stat.name === nature.increasedStat) {
+                const selectedNatureObj = natureData.find(n => n.name === nature)
+                if (selectedNatureObj) {
+                    if (stat.stat.name === selectedNatureObj.increasedStat) {
                         multiplier = 1.1;
                     }
-                    else if (stat.stat.name === nature.decreasedStat){
+                    else if (stat.stat.name === selectedNatureObj.decreasedStat){
                         multiplier = 0.9;
                     }
                 }
-                baseStat = Math.floor(((stat.base_stat * 2 + 31 +(100 / 4)) * level / 100 +5) * multiplier)
+                baseStat = Math.floor(((stat.base_stat * 2 + iv +(ev / 4)) * level / 100 +5) * multiplier)
             }
             return {...stat, base_stat: baseStat};
         });
     };
 
-    const adjustedStats = calulateBaseStatsWithNatures(stats, pokemonLevel, natureData[0]);
+    const adjustedStats = calulateBaseStatsWithNatures(stats, pokemonLevel, currentNature);
     const handlePokemonLevelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPokemonLevel(parseInt(e.target.value));
     }
+    const handleNatureSelection = (e : React.ChangeEvent<HTMLSelectElement>) => {
+        setCurrentNature(e.target.value);
+    }
+    const handleIVChange = (statName: string, ivValue: number) => {
+        setIVS(preIVs => ({...preIVs, [statName]: ivValue}))
+    }
+    const handleEVChange = (statName: string, evValue: number) => {
+        setEVS(preEVs => ({...preEVs, [statName]: evValue}))
+    }
     return (
         <div>
-            <h3>Base Stats</h3>
-            <ul>
-                {adjustedStats.map((stat) => (
-                    <li key={stat.stat.name}>
-                        {stat.stat.name} : {stat.base_stat}
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <label htmlFor="nature">
+                    <p>Nature:</p>
+                    <select id="nature" value={currentNature} onChange={handleNatureSelection}>
+                        <option value="">None</option>
+                        {natureData.map(nature =>(
+                            <option key={nature.name} value={nature.name}>
+                                {nature.name}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+            </div>
             <div>
                 <label htmlFor="level">Set Pokemon Level : {pokemonLevel}</label>
                     <input 
@@ -94,6 +114,25 @@ const PokemonStatsRenderer : React.FC<StatsProp> = ({stats}) => {
                         onChange={handlePokemonLevelChange}
                     />
             </div>
+            <div>
+                <h3>IVs and EVs</h3>
+                <ul>
+                    {stats.map(stat => (
+                        <li key={stat.stat.name}>
+                            {stat.stat.name} : IV <input type="number" value={ivs[stat.stat.name] || ""} onChange={(e) => handleIVChange(stat.stat.name, parseInt(e.target.value))}/>
+                            {stat.stat.name} : EV <input type="number" value={evs[stat.stat.name] || ""} onChange={(e) => handleEVChange(stat.stat.name, parseInt(e.target.value))}/>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <h3>Total Stats</h3>
+            <ul>
+                {adjustedStats.map((stat) => (
+                    <li key={stat.stat.name}>
+                        {stat.stat.name} : {stat.base_stat}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
